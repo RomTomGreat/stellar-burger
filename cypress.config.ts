f5419e2ad288dd40cb3dd5186a6dca8906fb0868
Tomin Roman <tominroman128@gmail.com>
2024-07-07 16:53:27 +0300

    create jest and cypress for next testing stellarBurger

---
+++ cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:4000',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      config.env = {
+        ...process.env,
+        ...config.env
+      }
+      return config;
+    },
+  },
+});
